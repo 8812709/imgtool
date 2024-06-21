95d50096fd542a7f02e046df07472202661c6468
rakib hussain <rakibhussain8812@gmail.com>
2024-06-21 10:54:01 +0530

    simplified the code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image
-import io
 
 app = FastAPI()
 
@@ -13,9 +12,7 @@ def info(name:str,roll_no:int):
 @app.post("/resize")
 async def resize_image(name:str,roll_no:int,passportphoto:UploadFile=File()):
     details={"name":name,"roll_no":roll_no,"message":"The resized image has been saved in the imgtool folder of this PC by a new name as newimage1.png"}
-    file_info = await passportphoto.read() # reading the image i uploaded and saves its info in file_info
-    image_data = io.BytesIO(file_info) # Use BytesIO to create a file-like object containing the image data
-    img = Image.open(image_data) # Open the image using syntax "open" which fetch the image data through image_data
+    img = Image.open(passportphoto.file) # Open the image using syntax "open" which fetch the image data through the uploaded file
     resized_image = img.resize((300,300)) # Resize the image to 300*300 ratio using the syntax "resize"
     resized_image.save("newimage1.png")  #saves the image in my pc in imgtool folder
     return(details)  #returns the details which student has given and delivers a message alongwith
