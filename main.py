4cd08cc0be22a25513d56f173938ed63724d8edd
rakib hussain <rakibhussain8812@gmail.com>
2024-06-23 12:12:58 +0530

    changed to default parameters

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def info(name:str,roll_no:int):
         raise HTTPException(status_code=404,details="some error occured while processing:{str(e)}")
 
 @app.post("/resize")
-async def resize_image(name:str,roll_no:int,passportphoto:UploadFile=File()):
+async def resize_image(name:str,roll_no:int,passportphoto:UploadFile=File(),width=300,height=300):
     details={"name":name,"roll_no":roll_no,"message":"The resized image has been saved in the imgtool folder of this PC by a new name as newimage1.png"}
     try:
         img = Image.open(passportphoto.file) # Open the image using syntax "open" which fetch the image data through the uploaded file
@@ -20,7 +20,7 @@ async def resize_image(name:str,roll_no:int,passportphoto:UploadFile=File()):
     except Exception as e:
         raise HTTPException(status_code=401,details="some error occured while opening the file:{str(e)}")
     try:
-        resized_image = img.resize((300,300)) # Resize the image to 300*300 ratio using the syntax "resize"
+        resized_image = img.resize((width,height)) # Resize the image to 300*300 ratio using the syntax "resize"
     except Exception as e:
         raise HTTPException(status_code=402,detail="some kind of error while resizing the image:{str(e)}")
     try:
--- pillow.py
+++ pillow.py
@@ -6,8 +6,8 @@ print(img.size)
 print(img.format)
 resize_1=(300,300)#the size i want in my new image
 img.thumbnail(resize_1) #using thumbnail syntax to resize my orginal image,but it maintains the aspect ratio of the image
-img.save("genshin_new.png") #using save sytax to save my new image as the follwing name
-img.resize(resize_1)#it will anyhow give me the exact height and width in the new image
+img.save("genshin_new.png") #using save sytax to save my new image as the follwing name in the same folder
+img.resize(resize_1)#using syntax resize which doesnt maintains the aspect ratio
 img.save("new_image.png")
 new_img=Image.open("new_image.png")
 print(new_img.size)
